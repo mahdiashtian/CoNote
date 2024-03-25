05b81c2420bbb709ea7df449121c6d52bfd7dca7
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-03-25 20:09:23 +0330

    Add JWT for websocket

--- users/security.py
+++ users/security.py
@@ -1,6 +1,14 @@
+from channels.auth import AuthMiddlewareStack
+from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from django.utils import timezone
+from jwt import decode as jwt_decode
 from rest_framework_simplejwt.settings import api_settings as jwt_settings
+
+User = get_user_model()
 
 
 def set_jwt_access_cookie(response, access_token):
@@ -100,3 +108,34 @@ def set_jwt_cookies(response, access_token, refresh_token):
 
     set_jwt_access_cookie(response, access_token)
     set_jwt_refresh_cookie(response, refresh_token)
+
+
+@database_sync_to_async
+def get_user(validated_token):
+    try:
+        user = get_user_model().objects.get(id=validated_token["user_id"])
+        return user
+    except User.DoesNotExist:
+        return AnonymousUser()
+
+
+class JwtAuthMiddleware(BaseMiddleware):
+    def __init__(self, inner):
+        self.inner = inner
+
+    async def __call__(self, scope, receive, send):
+        headers = dict(scope['headers'])
+        if b'authorization' in headers:
+            token_name, token_key = headers[b'authorization'].decode().split()
+            if token_name.lower() == 'bearer':
+                decoded_data = jwt_decode(token_key, settings.SECRET_KEY, algorithms=["HS256"])
+                scope["user"] = await get_user(validated_token=decoded_data)
+            else:
+                scope["user"] = AnonymousUser()
+        else:
+            scope["user"] = AnonymousUser()
+        return await super().__call__(scope, receive, send)
+
+
+def JwtAuthMiddlewareStack(inner):
+    return JwtAuthMiddleware(AuthMiddlewareStack(inner))
